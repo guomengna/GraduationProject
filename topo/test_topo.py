81c72d84e1418d01d283b28c0708e56f9765d474
guomengna <guomengna19941209@163.com>
2018-10-26 21:45:34 +0800

    加入cernet_added，修改拓扑代码，边有加权边来代替

--- topo/test_topo.py
+++ topo/test_topo.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import xlrd
 import xlwt
 G = nx.Graph()
-ExcelFile = xlrd.open_workbook('cernet2.xlsx','r')
+ExcelFile = xlrd.open_workbook('cernet2_added.xlsx','r')
 #sheet1 = ExcelFile.sheet_names()
 nums = len(ExcelFile.sheets())
 for i in range(nums):
@@ -13,14 +13,23 @@ for i in range(nums):
 nrows = sheet1.nrows   #行
 ncols = sheet1.ncols   #列
 print(nrows,ncols)
+
 #循环行列表数据
 for i in range(nrows):
-    print(sheet1.row_values(i))
+    #print(sheet1.row_values(i))
     list = sheet1.row_values(i)
-    print(int(list[0]),int(list[1]))
+    list1 = [str(j) for j in list]
+    #print(int(list[0]), int(list[1]))
+    #print(list1[0], list1[1])
+    #print(int(list[0]),int(list[1]))
     G.add_node(int(list[0]))
     G.add_node(int(list[1]))
-    G.add_edge(list[0],list[1])
+    #G.add_edge(list[0],list[1])
+    #添加带有权值的边
+    G.add_weighted_edges_from([(list[0], list[1], list[8])])
+    #获取边的权值
+    print(G.get_edge_data(list[0], list[1]))
+
 plt.figure()
 nx.draw(G, node_color='r', with_labels=True, node_size=800, font_color='w')
 plt.show()
