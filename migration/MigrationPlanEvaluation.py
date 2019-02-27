955d1889a45441fc4283a81935fd9ad41d41def0
guomengna <guomengna19941209@163.com>
2019-02-27 20:22:55 +0800

    完成迁移方案评价类

--- migration/MigrationPlanEvaluation.py
+++ migration/MigrationPlanEvaluation.py
@@ -1,1 +1,26 @@
 """迁移方案评价"""
+from entity.SFC import SFC
+from entity.SFCList import sfcListSingleton
+from migration.MigrationCostCaculation import MigrationCostCaculation
+
+class MigrationPlanEvaluation():
+    """迁移方案评价方法"""
+    def __init__(self, migrated_SFC_id, SFCDelayBeforMigration,
+                 SFCRequestedResourceBefore, needMigratedVNFList, destinationPhysicalNodeList):
+        self.migrated_SFC_id = migrated_SFC_id
+        self.SFCDelayBeforMigration = SFCDelayBeforMigration
+        self.SFCRequestedResourceBefore = SFCRequestedResourceBefore
+        self.needMigratedVNFList = needMigratedVNFList
+        self.destinationPhysicalNodeList = destinationPhysicalNodeList
+
+    def evaluation(self):
+        totalRelibility = 0
+        for SFCId in sfcListSingleton:
+            SFCInstance = SFC(SFCId)
+            totalRelibility += SFCInstance.get_SFC_relialibility(SFCInstance.getVNFList())
+
+        costInstance = MigrationCostCaculation()
+        cost = costInstance.getCostOfMigratingVNFsOnOneSFC(self.migrated_SFC_id, self.SFCDelayBeforMigration,
+                                                    self.SFCRequestedResourceBefore, self.needMigratedVNFList,
+                                                    self.destinationPhysicalNodeList)
+        return 100 + totalRelibility - cost
